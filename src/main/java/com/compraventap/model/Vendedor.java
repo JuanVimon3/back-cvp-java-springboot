7c0accc5e49460e3bbfe3323a0586643a5218810
JuanVimon3 <jvillamonsalve@gmail.com>
2026-05-19 16:40:39 -0500

    WIP

--- src/main/java/com/compraventap/model/Vendedor.java
+++ src/main/java/com/compraventap/model/Vendedor.java
@@ -1,7 +1,5 @@
 package com.compraventap.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
